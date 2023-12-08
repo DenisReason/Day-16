828d50ae73f1d7fc13af87f1b785a7bf0fa08ff9
MinhTri <str.comparetoignore@gmail.com>
2023-12-08 20:50:36 +0700

    add post

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 const io = new Server(server,{
     cors:{
     origin:"http://localhost:19006",
-    methods:["GET,PUSH"],
+    methods:["GET,POST"],
     credentials:true
 }});
 
